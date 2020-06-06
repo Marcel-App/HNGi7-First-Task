bbe881af9f98b1fbec9e48d929b292106b421e75
Marcel-App <66368225+Marcel-App@users.noreply.github.com>
2020-06-06 23:35:01 +0100

    Created JavaScript file
    Made a JavaScript file using JavaScript for stage 2 task

---
+++ scripts/Marcel.js
@@ -0,0 +1,7 @@
+Var name = "Marcellinus Obiajunwa"
+Var hngId ="HNG-06425"
+Var currentlanguage ="JavaScript"
+Var email ="marcellinusobiajunwa@gmail.com"
+Var output=`Hello World,this is ${name} with HNGi7 ID ${hngId} and email ${email} using ${currentlanguage} for stage 2 task`;
+
+console.log(output);
